a8fe5eab4f9256ced4b4d3dbaba637d5b787ef79
Alex Wilson <acwilson96@protonmail.com>
2017-09-20 00:49:49 +0100

    Video Tutorial 2

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const schema            = require('./schema.js');
 
 const app               = express();
 
-app.use('/graphql'), expressGraphQL({
+app.use('/graphql', expressGraphQL({
     schema: schema,
     graphiql: true
-});
+}));
 
 app.listen(4000, () => { console.log('Server is running on port:4000'); });
